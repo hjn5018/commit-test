b3f41d6ef08ce6652a60f4c53f8dd876535ab772
hjn5018 <hjn5018@gmail.com>
2024-09-11 18:22:23 +0900

    Add feature/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from accounts.models import User
 
-# Create your views here.
+@csrf_exempt
+def signup(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    User.objects.create_user(username=username, password=password)
+    return JsonResponse({'message': 'signup completed'})
